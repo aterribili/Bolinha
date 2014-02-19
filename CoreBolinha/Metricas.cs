3d46ae743ba429f09780309df4ae1c050fb75f0d
aterribili <abner.terribili@gmail.com>
2014-02-19 16:34:21 -0300

    Teste de listagem de commits por arquivos implementado

--- CoreBolinha/Metricas.cs
+++ CoreBolinha/Metricas.cs
@@ -15,5 +15,26 @@ namespace CoreBolinha
             return nomesArquivos.Select((nome) => File.ReadAllLines(nome).Length).ToList();
         }
 
+        private List<String> PegaNomeArquivosAlteradosPorCommit(Commit commit)
+        {
+            commit.Message.Select((e) => e);
+            return commit.Tree.Select((a) => a.Name).ToList();
+        }
+
+        public List<Arquivo> CalculaQuantidadeVezesQueArquivoFoiAlterado(IQueryableCommitLog commits)
+        {
+
+            var nomes = commits.SelectMany((commit) => PegaNomeArquivosAlteradosPorCommit(commit));
+
+            return nomes.
+                OrderBy((elemento) => elemento).
+                Distinct().
+                Select((nome) => new Arquivo(nome, ContaOcorrenciasNomeArquivo(nomes, nome))).ToList();
+        }
+
+        private int ContaOcorrenciasNomeArquivo(IEnumerable<string> nomes, string nome)
+        {
+            return nomes.Where((arquivo) => nome == arquivo).Count();
+        }
     }
 }
