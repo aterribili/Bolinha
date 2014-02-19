3a0fa2e647c37e347933e530fa14cc1e6c8f3cf1
aterribili <abner.terribili@gmail.com>
2014-02-19 16:35:48 -0300

    Teste de listagem de commits por arquivos implementado

---
+++ CoreBolinha/Arquivo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreBolinha
+{
+    public class Arquivo
+    {
+        private readonly String nome;
+        private readonly int alterado;
+
+        public Arquivo(String nome, int vezesAlterado)
+        {
+            this.nome = nome;
+            this.alterado = vezesAlterado;
+        }
+
+        public override string ToString()
+        {
+            return "Arquivo "+nome+" alterado "+alterado.ToString()+" vezes";
+        }
+
+    }
+}
