f88a0c4568fd5e0678379ccd20c151ddab1afd6d
aterribili <abner.terribili@gmail.com>
2014-02-19 16:33:55 -0300

    Teste de listagem de commits por arquivos implementado

--- CoreBolinhaTest/MetricasTest.cs
+++ CoreBolinhaTest/MetricasTest.cs
@@ -5,6 +5,7 @@ using LibGit2Sharp;
 using System.IO;
 using System.Security.Permissions;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace CoreBolinhaTest
 {
@@ -42,6 +43,49 @@ namespace CoreBolinhaTest
             Assert.AreEqual(3, metricas.CalculaQuantidadeLinhasDosArquivos(listaArquivos)[0]);
             Assert.AreEqual(3, metricas.CalculaQuantidadeLinhasDosArquivos(listaArquivos)[1]);
             Assert.AreEqual(3, metricas.CalculaQuantidadeLinhasDosArquivos(listaArquivos)[2]);
+        }
+
+        [TestMethod]
+        public void DeveListarCommitsPorArquivo()
+        {
+            var destinoPath = PathAleatorio();
+            var origemPath = Repository.Init(destinoPath, false);
+
+            using (var repo = new Repository(origemPath))
+            {
+                File.WriteAllText(destinoPath + "\\arquivo.txt", "Lambda\r\nLambda\r\nLambda");
+                File.WriteAllText(destinoPath + "\\arquivo-2.txt", "Teste\r\nTeste\r\nTeste");
+
+                repo.Index.Stage("arquivo.txt");
+                repo.Commit("Adicionando arquivo");
+                repo.Index.Unstage("arquivo.txt");
+            }
+            using (var repo = new Repository(origemPath))
+            {
+                repo.Index.Stage("arquivo-2.txt");
+                repo.Commit("Adicionando arquivo-2");
+            }
+            using (var repo = new Repository(origemPath))
+            {
+                File.WriteAllText(destinoPath + "\\arquivo-2.txt", "T\r\nT\r\nT");
+                repo.Index.Stage("arquivo-2.txt");
+                repo.Commit("Adicionando arquivo-2");
+            }
+            using (var repo = new Repository(origemPath))
+            {
+                File.WriteAllText(destinoPath + "\\arquivo-3.txt", "Blz\r\nBlz\r\nBlz");
+                repo.Index.Stage("arquivo-3");
+
+                File.WriteAllText(destinoPath + "\\arquivo-4.txt", "Blz\r\nBlz\r\nBlz");
+                repo.Index.Stage("arquivo-4.txt");
+                repo.Commit("Adicionando arquivo-3 e arquivo-4");
+            }
+            using (var repo = new Repository(origemPath))
+            {
+                Assert.AreEqual(4, repo.Commits.Count());
+                Assert.AreEqual("Arquivo arquivo.txt alterado 1 vezes", new Metricas().CalculaQuantidadeVezesQueArquivoFoiAlterado(repo.Commits).ElementAt(0).ToString());
+            }
+
         }
     }
 }
